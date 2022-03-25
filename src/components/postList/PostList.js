22c1cf1a63b0a86bb7d638b63653bc2b8e982cdc
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2022-03-25 13:46:40 +0200

    Created postList component and fetchPosts action creator, called the action creator inside useEffect hook inside postList and restructured the folders of the app

---
+++ src/components/postList/PostList.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../../actions";
+
+function PostList() {
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return <div>SongList</div>;
+}
+
+export default connect(null, { fetchPosts })(PostList);
